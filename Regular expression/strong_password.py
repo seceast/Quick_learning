cbdca8eec517f82d0bf18ad92803936efa4a8061
seceast <yangyadong25@163.com>
2020-05-25 23:02:29 +0800

    add file

--- Regular expression/strong_password.py
+++ Regular expression/strong_password.py
@@ -7,21 +7,23 @@ import re
 
 
 def strong_pwd(pwd: str):
-    count = 0
-    if len(pwd) < 8:
-        return False
-    if re.search(r'[a-z]+', pwd) :
-        count += 1
-    if re.search(r'[A-Z]+', pwd):
-        count += 1
-    if re.search(r'[0-9]+', pwd):
-        count += 1
-    if count == 3:
+    # count = 0
+    # if len(pwd) < 8:
+    #     return False
+    # if re.search(r'[a-z]+', pwd):
+    #     count += 1
+    # if re.search(r'[A-Z]+', pwd):
+    #     count += 1
+    # if re.search(r'[0-9]+', pwd):
+    #     count += 1
+    # if count == 3:
+    #     return True
+    if re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\s\S]{8,}$', pwd):
         return True
     return False
 
 
-a = 'dad123as'
-b = '1231asAS'
+a = 'daA1as'
+b = '1231asAS11dad'
 
 print(strong_pwd(a), strong_pwd(b))
