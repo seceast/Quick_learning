276f6c18cbc1ce27e7971f4452a500d96660045f
seceast <yangyadong25@163.com>
2020-05-25 22:18:07 +0800

    add file

--- Regular expression/strong_password.py
+++ Regular expression/strong_password.py
@@ -7,6 +7,21 @@ import re
 
 
 def strong_pwd(pwd: str):
+    count = 0
     if len(pwd) < 8:
         return False
-    if
+    if re.search(r'[a-z]+', pwd) :
+        count += 1
+    if re.search(r'[A-Z]+', pwd):
+        count += 1
+    if re.search(r'[0-9]+', pwd):
+        count += 1
+    if count == 3:
+        return True
+    return False
+
+
+a = 'dad123as'
+b = '1231asAS'
+
+print(strong_pwd(a), strong_pwd(b))
